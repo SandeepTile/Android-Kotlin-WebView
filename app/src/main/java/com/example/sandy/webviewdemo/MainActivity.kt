a3005320a12b426068fd5ddc9edb49f4fbd4826c
sandeeptile <sandeeptile@gmail.com>
2018-07-18 22:10:15 +0530

    button working

--- app/src/main/java/com/example/sandy/webviewdemo/MainActivity.kt
+++ app/src/main/java/com/example/sandy/webviewdemo/MainActivity.kt
@@ -20,6 +20,10 @@ class MainActivity : AppCompatActivity() {
         when(v.id){
 
             R.id.search->wview.loadUrl(et1.text.toString())
+            R.id.fb->wview.loadUrl("http://www.facebook.com")
+            R.id.google->wview.loadUrl("http://www.google.com")
+            R.id.ytube->wview.loadUrl("http://www.youtube.com")
+
         }
     }
 }
